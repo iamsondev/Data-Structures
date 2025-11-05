26d6a4e49def99dd3bff26893ed2a7de70fda28f
iamsondev <sondipkumarsk@gmail.com>
2025-11-05 13:24:43 +0600

    implementation prepend

--- linkedListAppend.js
+++ linkedListAppend.js
@@ -21,7 +21,17 @@ class LinkedList {
     }
     this.length++;
   }
-  prepend() {}
+  prepend(value) {
+    const newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+  }
   insert() {}
   remove() {}
   print() {
@@ -39,5 +49,9 @@ const linkedList = new LinkedList();
 linkedList.append(1);
 linkedList.append(2);
 linkedList.append(3);
+
+linkedList.prepend(10);
+linkedList.prepend(20);
+linkedList.prepend(30);
 
 linkedList.print();
